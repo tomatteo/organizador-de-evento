6bc71351e1cde6d8b6dd805ae523378ca9017405
João Vitor Raimundi <joaoraimundi@gmail.com>
2025-11-18 20:18:02 -0300

    Criação de Classes
    Criação das classes relatório e usuários

---
+++ relatorio.py
@@ -0,0 +1,12 @@
+class Relatorio:
+    def __init__(self, id: int, conteudo: str):
+        self._id = id
+        self._dataGeracao = datetime.now()
+        self._conteudo = conteudo
+    
+    @property
+    def conteudo(self) -> str: return self._conteudo
+
+    def gerarResumo(self) -> str:
+        return (f"Relatório ID {self._id} (Gerado em: {self._dataGeracao.strftime('%Y-%m-%d')})\n"
+                + "="*20 + f"\n{self.conteudo}")
